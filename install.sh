bcf435c6a39ef7e9ffa672f9832fa7de06972bcd
g4xd <32615076+g4xd@users.noreply.github.com>
2017-10-29 01:14:57 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -66,7 +66,7 @@ mount /dev/sda2 /boot
 # Configuring Portage
 emerge-webrsync
 emerge cpuid2cpuflags
-sed -i "s/^CPU_FLAGS_X86.*/`cpuinfo2cpuflags-x86`/" /etc/portage/make.conf
+sed -i "s/^CPU_FLAGS_X86.*/\$(cpuinfo2cpuflags-x86)/" /etc/portage/make.conf
 sed -i "/^CFLAGS/s/\".*\"/\"-march=native -O2 -pipe\"/" /etc/portage/make.conf
 sed -i "/^USE/s/\".*\"/\"X vulkan vaapi alsa xtf\"/" /etc/portage/make.conf
 echo "MAKEOPTS=\"-j$(nproc)\"" >> /etc/portage/make.conf
