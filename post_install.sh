c314356ac45007b5ab12aab783dd514ca4e7b8fd
g4xd <32615076+g4xd@users.noreply.github.com>
2017-10-30 12:02:45 +0300

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -10,7 +10,8 @@ echo "x11-libs/libxcb xkb\nx11-libs/cario xcb" > /etc/portage/package.use/i3
 emerge x11-base/xorg-drivers \
   x11-base/xorg-server \
   x11-terms/rxvt-unicode \
-  media-libs/vulkan-loader
+  media-libs/vulkan-loader \
+  media-sound/alsa-utils
 emerge x11-wm/i3 \
   x11-misc/i3status \
   x11-misc/i3lock \
